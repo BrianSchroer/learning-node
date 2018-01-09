867746ae1eddb9b239fd0ca644604de9e1c27251
Brian Schroer <brianschroer@gmail.com>
2018-01-09 09:35:17 -0600

    Completed exercise11

---
+++ exercise11/program.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = process.argv[2];
+const filePath = process.argv[3];
+
+const server = http.createServer(handleRequest);
+
+function handleRequest(request, response) {
+  response.writeHead(200, { 'content-type': 'text/plain' });
+  fs.createReadStream(filePath).pipe(response);
+}
+
+server.listen(port);
