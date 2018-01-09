da59651708f4613fe7d18a4b04054338918d463f
Brian Schroer <brianschroer@gmail.com>
2018-01-09 14:40:22 -0600

    Completed mongo-exercise03

---
+++ mongo-exercise03/program.js
@@ -0,0 +1,20 @@
+const mongo = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const dbName = 'learnyoumongo';
+const dbUrl = `mongodb://localhost:27017/${dbName}`;
+const minimumAge = parseInt(process.argv[2]);
+
+mongo.connect(dbUrl, (err, database) => {
+  assert(err == null);
+
+  const db = database.db(dbName);
+  const parrots = db.collection('parrots');
+
+  parrots.find({ age: { $gt: minimumAge } }).toArray((err, docs) => {
+    assert(err == null);
+    console.log(docs);
+  });
+
+  database.close();
+});
