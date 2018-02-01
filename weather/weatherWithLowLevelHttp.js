f5bdf4c866110466a5c6cdca3ddb4582d8f053ff
Brian Schroer <brianschroer@gmail.com>
2018-02-01 13:17:44 -0600

    Moved node-university excercises to their own folder

--- weather/weatherWithLowLevelHttp.js
+++ weather/weatherWithLowLevelHttp.js
@@ -6,25 +6,25 @@ const apiKey = '78c2d3ab80e4660131d13324e1d0575d';
 
 const url = `${openWeatherMapUrl}?zip=${zipCode}&appid=${apiKey}&units=imperial`;
 
-http.get(url, (res) => {
-  let body = '';
+http
+  .get(url, res => {
+    let body = '';
 
-  res.on('data', (data) => {
-    body += data;
+    res.on('data', data => {
+      body += data;
+    });
+
+    res.on('end', () => {
+      const json = JSON.parse(body);
+
+      if (json.cod != 200) {
+        console.log(`Error retrieving weather data: ${json.cod}`);
+        return;
+      }
+
+      console.log(`${json.main.temp}F, ${json.weather[0].description}`);
+    });
   })
-
-  res.on('end', () => {
-    const json = JSON.parse(body);
-
-    if (json.cod != 200) {
-      console.log(`Error retrieving weather data: ${json.COD}`);
-      return;
-    }
-
-    console.log(`${json.main.temp}F, ${json.weather[0].description}`);
+  .on('error', e => {
+    console.log(`Error: ${e.message}`);
   });
-
-
-}).on('error', (e) => {
-  console.log(`Error: ${e.message}`);
-});
